4687c0091d3649db0e931696e8022370ec4a090f
Thomas Grubbe Sandborg Lauritsen <Laurits1994@gmail.com>
2021-01-31 18:10:11 +0100

    day4 part 1 solved

--- src/AOCbase/day4.java
+++ src/AOCbase/day4.java
@@ -14,8 +14,25 @@ public class day4 extends AOCAssignmentBase{
         String[] validPassportKeys = new String[]{"byr","iyr","eyr","hgt","hcl","ecl","pid"};
         ArrayList<String> currentPassportKeys = new ArrayList<>();
 
-        for(String line: lines){
-            if (line.isBlank()){
+        for(int i = 0; i < lines.size(); i++){
+            if (lines.get(i).isBlank()){
+                boolean validPassword = true;
+                for (String key: validPassportKeys){
+                    if (!currentPassportKeys.contains(key)){
+                        validPassword = false;
+                        break;
+                    }
+                }
+                if (validPassword){
+                    ans++;
+                }
+                currentPassportKeys.clear();
+            }
+            else if (lines.get(i).equals(lines.get(lines.size() - 1)) && !lines.get(i).isBlank()){
+                String[] lineKeys = lines.get(i).split(" ");
+                for(String part: lineKeys){
+                    currentPassportKeys.add(part.split(":")[0]);
+                }
                 boolean validPassword = true;
                 for (String key: validPassportKeys){
                     if (!currentPassportKeys.contains(key)){
@@ -29,12 +46,17 @@ public class day4 extends AOCAssignmentBase{
                 currentPassportKeys.clear();
             }
             else{
-                String[] lineKeys = line.split(" ");
+                String[] lineKeys = lines.get(i).split(" ");
                 for(String part: lineKeys){
                     currentPassportKeys.add(part.split(":")[0]);
                 }
             }
         }
         System.out.println(ans);
+        solve2();
+    }
+
+    void solve2(){
+
     }
 }
