0a53eb2ee2e25d3036976c095a486c2e26e45064
Thomas Grubbe Sandborg Lauritsen <Laurits1994@gmail.com>
2021-01-31 17:44:30 +0100

    day4 progress

--- src/AOCbase/day4.java
+++ src/AOCbase/day4.java
@@ -1,6 +1,7 @@
 package AOCbase;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class day4 extends AOCAssignmentBase{
     public day4(String day) throws IOException {
@@ -9,6 +10,31 @@ public class day4 extends AOCAssignmentBase{
 
     @Override
     void solve() {
+        int ans = 0;
+        String[] validPassportKeys = new String[]{"byr","iyr","eyr","hgt","hcl","ecl","pid"};
+        ArrayList<String> currentPassportKeys = new ArrayList<>();
 
+        for(String line: lines){
+            if (line.isBlank()){
+                boolean validPassword = true;
+                for (String key: validPassportKeys){
+                    if (!currentPassportKeys.contains(key)){
+                        validPassword = false;
+                        break;
+                    }
+                }
+                if (validPassword){
+                    ans++;
+                }
+                currentPassportKeys.clear();
+            }
+            else{
+                String[] lineKeys = line.split(" ");
+                for(String part: lineKeys){
+                    currentPassportKeys.add(part.split(":")[0]);
+                }
+            }
+        }
+        System.out.println(ans);
     }
 }
